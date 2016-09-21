587dce4bb7b8e3a64029103285a4c7b705239a85
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-09-21 15:37:22 +0200

    fix prepublish

--- internals/prepublish.sh
+++ internals/prepublish.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
 
-echo_fail() {
-  echo $1
-  exit 1
-}
-
-isValidReleaseType () {
-  validTypes=("major" "minor" "patch")
-  local e
-  for e in "${validTypes[@]}"; do [[ $e == $1 ]] && return 1; done
-  return 0
-}
+./internals/runTestSuite.sh
+./internals/buildPackage.sh
