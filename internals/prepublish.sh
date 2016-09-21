3f13e801e0d3497e640d1fd2e89167f6c9a84107
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-09-21 15:29:54 +0200

    no version bump in prepublish

--- internals/prepublish.sh
+++ internals/prepublish.sh
@@ -11,19 +11,3 @@ isValidReleaseType () {
   for e in "${validTypes[@]}"; do [[ $e == $1 ]] && return 1; done
   return 0
 }
-
-echo
-echo 'Release gRPC Event Store on NPM'
-echo
-./internals/runTestSuite.sh
-./internals/buildPackage.sh
-echo
-echo 'Type the release type (major || minor || patch)'
-read -p "> " RELEASE_TYPE
-
-isValidReleaseType $RELEASE_TYPE
-VALID_TYPE=$?
-
-[[ $VALID_TYPE == 1 ]] || echo_fail "Release type not valid"
-
-npm version "$RELEASE_TYPE"
