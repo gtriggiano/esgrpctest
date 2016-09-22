694b05fb0be1c49178907632558d9b9b13ff3b39
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-09-22 18:00:36 +0200

    .

--- tests/unit.js
+++ tests/unit.js
@@ -1,16 +1,21 @@
 import should from 'should/as-function'
-import sinon from 'sinon'
+import EventEmitter from 'eventemitter3'
 
 import EventStore from '../src'
 import ServiceNode from '../src/ServiceNode'
 
-describe('gRPC Event Store Package', function () {
+describe('gRPC Event Store Package Unit Tests', function () {
   it('should be fun to work with', () => {})
   it(`let EventStore = require('grpc-event-store'); typeof EventStore === 'function'`, () => {
     should(EventStore).be.a.Function()
   })
-  it(`let eventStoreNode = EventStore(host [, settings]); eventStoreNode instanceof ServiceNode === true`, () => {
-    let eventStoreNode = EventStore('development')
-    should(eventStoreNode).be.an.instanceof(ServiceNode)
+  it(`let eventStore = EventStore(settings); eventStore instanceof EventEmitter === true`, () => {
+    let eventStore = EventStore('development')
+    should(eventStore).be.an.instanceof(EventEmitter)
   })
+
+  require('../src/_unitTestBackendInterface')
+  require('../src/_unitTestStoreInterface')
+  require('../src/_unitTestGRPCInterface')
+  require('../src/_unitTestServiceNode')
 })
