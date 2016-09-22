aea4da81e93d7bb3cdba6462c799eb1bc3fedab6
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-09-22 18:28:49 +0200

    wiring dnsmq-messagebus

--- src/StoreInterface.js
+++ src/StoreInterface.js
@@ -1,18 +1,39 @@
 import { merge } from 'lodash'
-import { prefixString } from './utils'
+import EventEmitter from 'eventemitter3'
+import MessageBus from 'dnsmq-messagebus'
+
+import { prefixString, isValidHost, isValidPort } from './utils'
 
 function StoreInterface (_settings) {
   let settings = merge({}, defaultSettings, _settings)
   _validateSettings(settings)
+
+  let {
+    host,
+    coordinationPort
+  } = settings
+
+  let store = new EventEmitter()
+
+  let _bus = MessageBus(host, {coordinationPort})
+  Object.defineProperty(store, 'bus', {value: _bus})
+
+  return store
 }
 
 const defaultSettings = {
-
+  coordinationPort: 50061
 }
 
 const iMsg = prefixString('[gRPC EventStore StoreInterface]: ')
 function _validateSettings (settings) {
+  let {
+    host,
+    coordinationPort
+  } = settings
 
+  if (!isValidHost(host)) throw new TypeError(iMsg('settings.host should be a valid string'))
+  if (!isValidPort(coordinationPort)) throw new TypeError(iMsg('settings.coordinationPort should be a positive integer'))
 }
 
 export default StoreInterface
