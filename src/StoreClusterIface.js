9c87b8e6481e7f9b8ff1465d0602de68ba9d94ef
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-09-22 02:00:18 +0200

    StoreClusterIface blueprint

---
+++ src/StoreClusterIface.js
@@ -0,0 +1,17 @@
+function StoreCluster (host, settings) {
+
+}
+
+const defaultSettings = {
+
+}
+
+const validateCtorInput = (host, settings) => {
+
+}
+
+export default StoreCluster
+export {
+  defaultSettings,
+  validateCtorInput
+}
