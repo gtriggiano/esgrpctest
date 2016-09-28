6417c8bb644ff28bdc59f39ced7404dcd1d9c071
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-09-28 18:00:31 +0200

    renaming api

--- src/GRPCImplementation/index.js
+++ src/GRPCImplementation/index.js
@@ -1,17 +1,17 @@
 // Import service methods
 import Ping from './Ping'
-import ReadAggregateStreamForwardFromCursor from './ReadAggregateStreamForwardFromCursor'
-import ReadAggregateTypeStreamForwardFromCursor from './ReadAggregateTypeStreamForwardFromCursor'
-import ReadEventTypeStreamForwardFromCursor from './ReadEventTypeStreamForwardFromCursor'
-import ReadStoreStreamForwardFromCursor from './ReadStoreStreamForwardFromCursor'
+import ReadAggregateStreamForwardFromVersion from './ReadAggregateStreamForwardFromVersion'
+import ReadAggregateTypeStreamForwardFromEvent from './ReadAggregateTypeStreamForwardFromEvent'
+import ReadEventTypeStreamForwardFromEvent from './ReadEventTypeStreamForwardFromEvent'
+import ReadStoreStreamForwardFromEvent from './ReadStoreStreamForwardFromEvent'
 import SubscribeToAggregateStream from './SubscribeToAggregateStream'
-import SubscribeToAggregateStreamFromCursor from './SubscribeToAggregateStreamFromCursor'
+import SubscribeToAggregateStreamFromVersion from './SubscribeToAggregateStreamFromVersion'
 import SubscribeToAggregateTypeStream from './SubscribeToAggregateTypeStream'
-import SubscribeToAggregateTypeStreamFromCursor from './SubscribeToAggregateTypeStreamFromCursor'
+import SubscribeToAggregateTypeStreamFromEvent from './SubscribeToAggregateTypeStreamFromEvent'
 import SubscribeToEventTypeStream from './SubscribeToEventTypeStream'
-import SubscribeToEventTypeStreamFromCursor from './SubscribeToEventTypeStreamFromCursor'
+import SubscribeToEventTypeStreamFromEvent from './SubscribeToEventTypeStreamFromEvent'
 import SubscribeToStoreStream from './SubscribeToStoreStream'
-import SubscribeToStoreStreamFromCursor from './SubscribeToStoreStreamFromCursor'
+import SubscribeToStoreStreamFromEvent from './SubscribeToStoreStreamFromEvent'
 import WriteToAggregateStream from './WriteToAggregateStream'
 import WriteToMultipleAggregateStream from './WriteToMultipleAggregateStream'
 
@@ -19,18 +19,18 @@ function GRPCImplementationFactory ({backend, store}) {
   let interfaces = {backend, store}
   return {
     ping: Ping(interfaces),
-    readAggregateStreamForwardFromCursor: ReadAggregateStreamForwardFromCursor(interfaces),
-    readAggregateTypeStreamForwardFromCursor: ReadAggregateTypeStreamForwardFromCursor(interfaces),
-    readEventTypeStreamForwardFromCursor: ReadEventTypeStreamForwardFromCursor(interfaces),
-    readStoreStreamForwardFromCursor: ReadStoreStreamForwardFromCursor(interfaces),
+    readAggregateStreamForwardFromVersion: ReadAggregateStreamForwardFromVersion(interfaces),
+    readAggregateTypeStreamForwardFromEvent: ReadAggregateTypeStreamForwardFromEvent(interfaces),
+    readEventTypeStreamForwardFromEvent: ReadEventTypeStreamForwardFromEvent(interfaces),
+    readStoreStreamForwardFromEvent: ReadStoreStreamForwardFromEvent(interfaces),
     subscribeToAggregateStream: SubscribeToAggregateStream(interfaces),
-    subscribeToAggregateStreamFromCursor: SubscribeToAggregateStreamFromCursor(interfaces),
+    subscribeToAggregateStreamFromVersion: SubscribeToAggregateStreamFromVersion(interfaces),
     subscribeToAggregateTypeStream: SubscribeToAggregateTypeStream(interfaces),
-    subscribeToAggregateTypeStreamFromCursor: SubscribeToAggregateTypeStreamFromCursor(interfaces),
+    subscribeToAggregateTypeStreamFromEvent: SubscribeToAggregateTypeStreamFromEvent(interfaces),
     subscribeToEventTypeStream: SubscribeToEventTypeStream(interfaces),
-    subscribeToEventTypeStreamFromCursor: SubscribeToEventTypeStreamFromCursor(interfaces),
+    subscribeToEventTypeStreamFromEvent: SubscribeToEventTypeStreamFromEvent(interfaces),
     subscribeToStoreStream: SubscribeToStoreStream(interfaces),
-    subscribeToStoreStreamFromCursor: SubscribeToStoreStreamFromCursor(interfaces),
+    subscribeToStoreStreamFromEvent: SubscribeToStoreStreamFromEvent(interfaces),
     writeToAggregateStream: WriteToAggregateStream(interfaces),
     writeToMultipleAggregateStream: WriteToMultipleAggregateStream(interfaces)
   }
