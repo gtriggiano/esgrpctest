ab1ce6723405408f68b47a45c1df290bb66a1b3d
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-10-03 17:50:24 +0200

    grpc api implementation

--- src/GRPCImplementation/storeQueries/ReadStoreStreamForwardFromEvent.js
+++ src/GRPCImplementation/storeQueries/ReadStoreStreamForwardFromEvent.js
@@ -1,5 +1,25 @@
+import { eventsStreamFromBackendEmitter } from '../../utils'
+
 function ReadStoreStreamForwardFromEvent ({backend}) {
-  return (call) => {}
+  return (call) => {
+    let {fromEventId, limit} = call.request
+
+    let params = {fromEventId}
+    if (limit > 0) params.limit = limit
+
+    let backendResults = backend.getEventsByTypes(params)
+    let eventsStream = eventsStreamFromBackendEmitter(backendResults)
+    let subscription = eventsStream.subscribe(
+      evt => call.write(evt),
+      err => call.emit('error', err),
+      () => call.end()
+    )
+
+    call.on('end', () => {
+      subscription.unsubscribe()
+      call.end()
+    })
+  }
 }
 
 export default ReadStoreStreamForwardFromEvent
--- src/GRPCImplementation/storeQueries/SubscribeToStoreStreamFromEvent.js
+++ src/GRPCImplementation/storeQueries/SubscribeToStoreStreamFromEvent.js
@@ -1,5 +1,41 @@
+import Rx from 'rxjs'
+
+import { eventsStreamFromBackendEmitter } from '../../utils'
+
 function SubscribeToStoreStreamFromEvent ({backend, store}) {
-  return (call) => {}
+  return (call) => {
+    let {fromEventId} = call.request
+
+    // Call backend
+    let params = {fromEventId}
+    let backendResults = backend.getEvents(params)
+    let backendStream = eventsStreamFromBackendEmitter(backendResults)
+
+    // Cache live events until backendStream ends
+    let replay = new Rx.ReplaySubject()
+    let cachedLiveStream = store.eventsStream.multicast(replay)
+    let cachedLiveStreamSubscription = cachedLiveStream.connect()
+    function _endCachedLiveStream () {
+      cachedLiveStreamSubscription.unsubscribe()
+      replay.complete()
+      // release memory
+      process.nextTick(() => replay._events.splice(0))
+    }
+    backendStream.toPromise().then(_endCachedLiveStream, _endCachedLiveStream)
+
+    // Concat the streams and subscribe
+    let eventsStream = backendStream.concat(cachedLiveStream, store.eventsStream)
+    let eventsStreamSubscription = eventsStream.subscribe(
+      evt => call.write(evt),
+      err => call.emit('error', err)
+    )
+
+    call.on('end', () => {
+      _endCachedLiveStream()
+      eventsStreamSubscription.unsubscribe()
+      call.end()
+    })
+  }
 }
 
 export default SubscribeToStoreStreamFromEvent
