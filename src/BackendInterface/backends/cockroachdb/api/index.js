c62c093ca1251c935076f26cc54ed9081e7dbe1d
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-10-03 17:51:50 +0200

    cockroach backend `getEvents` implementation

--- src/BackendInterface/backends/cockroachdb/api/index.js
+++ src/BackendInterface/backends/cockroachdb/api/index.js
@@ -1,8 +1,10 @@
+import getEvents from './getEvents'
 import getEventsByAggregate from './getEventsByAggregate'
 import getEventsByAggregateTypes from './getEventsByAggregateTypes'
 import getEventsByTypes from './getEventsByTypes'
 
 const apiHandlersFactories = {
+  getEvents,
   getEventsByAggregate,
   getEventsByAggregateTypes,
   getEventsByTypes
