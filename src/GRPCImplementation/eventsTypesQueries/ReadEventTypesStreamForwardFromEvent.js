20cef3619b3276af883a6e39ed831cae5bc80ce4
Giacomo Triggiano <giacomotriggiano@gmail.com>
2016-10-08 14:57:00 +0200

    ..

--- src/GRPCImplementation/eventsTypesQueries/ReadEventTypesStreamForwardFromEvent.js
+++ src/GRPCImplementation/eventsTypesQueries/ReadEventTypesStreamForwardFromEvent.js
@@ -1,4 +1,4 @@
-import { every } from 'lodash'
+import { every, max } from 'lodash'
 
 import { isValidString, eventsStreamFromBackendEmitter } from '../../utils'
 
@@ -9,6 +9,8 @@ function ReadEventTypesStreamForwardFromEvent ({backend}) {
     // Validate request
     if (!eventTypes.length) return call.emit('error', new TypeError('eventTypes should contain one or more non empty strings'))
     if (!every(eventTypes, isValidString)) return call.emit('error', new TypeError('every item of eventTypes should be a non empty string'))
+
+    fromEventId = max([0, fromEventId])
 
     let params = {eventTypes, fromEventId}
     if (limit > 0) params.limit = limit
